1d90442d20db5ac5dc759e15326c939617d1cc68
David Matthew <dianahdez.deleon@gmail.com>
2023-08-02 19:20:51 -0700

    Interprete basico
    Commits iniciales de un Interprete basico

---
+++ compiler.py
@@ -0,0 +1,518 @@
+import ply.lex as lex
+import ply.yacc as yacc
+from enum import Enum
+
+# ANALIZADOR LEXICO
+
+# -----------------------------------------------------------------------------
+# Rainman Sián
+# 26-02-2020
+#
+# Ejemplo interprete sencillo con Python utilizando ply en Ubuntu
+# -----------------------------------------------------------------------------
+
+reservadas = {
+    'numero' : 'NUMERO',
+    'imprimir' : 'IMPRIMIR',
+    'mientras' : 'MIENTRAS',
+    'if' : 'IF',
+    'else' : 'ELSE'
+}
+
+tokens  = [
+    'PTCOMA',
+    'LLAVIZQ',
+    'LLAVDER',
+    'PARIZQ',
+    'PARDER',
+    'IGUAL',
+    'MAS',
+    'MENOS',
+    'POR',
+    'DIVIDIDO',
+    'CONCAT',
+    'MENQUE',
+    'MAYQUE',
+    'IGUALQUE',
+    'NIGUALQUE',
+    'DECIMAL',
+    'ENTERO',
+    'CADENA',
+    'ID'
+] + list(reservadas.values())
+
+# Tokens
+t_PTCOMA    = r';'
+t_LLAVIZQ   = r'{'
+t_LLAVDER   = r'}'
+t_PARIZQ    = r'\('
+t_PARDER    = r'\)'
+t_IGUAL     = r'='
+t_MAS       = r'\+'
+t_MENOS     = r'-'
+t_POR       = r'\*'
+t_DIVIDIDO  = r'/'
+t_CONCAT    = r'&'
+t_MENQUE    = r'<'
+t_MAYQUE    = r'>'
+t_IGUALQUE  = r'=='
+t_NIGUALQUE = r'!='
+
+def t_DECIMAL(t):
+    r'\d+\.\d+'
+    try:
+        t.value = float(t.value)
+    except ValueError:
+        print("Float value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_ENTERO(t):
+    r'\d+'
+    try:
+        t.value = int(t.value)
+    except ValueError:
+        print("Integer value too large %d", t.value)
+        t.value = 0
+    return t
+
+def t_ID(t):
+     r'[a-zA-Z_][a-zA-Z_0-9]*'
+     t.type = reservadas.get(t.value.lower(),'ID')    # Check for reserved words
+     return t
+
+def t_CADENA(t):
+    r'\".*?\"'
+    t.value = t.value[1:-1] # remuevo las comillas
+    return t 
+
+# Comentario de múltiples líneas /* .. */
+def t_COMENTARIO_MULTILINEA(t):
+    r'/\*(.|\n)*?\*/'
+    t.lexer.lineno += t.value.count('\n')
+
+# Comentario simple // ...
+def t_COMENTARIO_SIMPLE(t):
+    r'//.*\n'
+    t.lexer.lineno += 1
+
+# Caracteres ignorados
+t_ignore = " \t"
+
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += t.value.count("\n")
+    
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+# Construyendo el analizador léxico
+lexer = lex.lex()
+
+class TIPO_DATO(Enum) :
+    NUMERO = 1
+
+class Simbolo() :
+    'Esta clase representa un simbolo dentro de nuestra tabla de simbolos'
+
+    def __init__(self, id, tipo, valor) :
+        self.id = id
+        self.tipo = tipo
+        self.valor = valor
+
+class TablaDeSimbolos() :
+    'Esta clase representa la tabla de simbolos'
+
+    def __init__(self, simbolos = {}) :
+        self.simbolos = simbolos
+
+    def agregar(self, simbolo) :
+        self.simbolos[simbolo.id] = simbolo
+    
+    def obtener(self, id) :
+        if not id in self.simbolos :
+            print('Error: variable ', id, ' no definida.')
+
+        return self.simbolos[id]
+
+    def actualizar(self, simbolo) :
+        if not simbolo.id in self.simbolos :
+            print('Error: variable ', simbolo.id, ' no definida.')
+        else :
+            self.simbolos[simbolo.id] = simbolo
+
+# Asociación de operadores y precedencia
+precedence = (
+    ('left','CONCAT'),
+    ('left','MAS','MENOS'),
+    ('left','POR','DIVIDIDO'),
+    ('right','UMENOS'),
+    )
+
+# Definición de la gramática
+
+
+
+def p_init(t) :
+    'init            : instrucciones'
+    t[0] = t[1]
+
+def p_instrucciones_lista(t) :
+    'instrucciones    : instrucciones instruccion'
+    t[1].append(t[2])
+    t[0] = t[1]
+
+
+def p_instrucciones_instruccion(t) :
+    'instrucciones    : instruccion '
+    t[0] = [t[1]]
+
+def p_instruccion(t) :
+    '''instruccion      : imprimir_instr
+                        | definicion_instr
+                        | asignacion_instr
+                        | mientras_instr
+                        | if_instr
+                        | if_else_instr'''
+    t[0] = t[1]
+
+def p_instruccion_imprimir(t) :
+    'imprimir_instr     : IMPRIMIR PARIZQ expresion_cadena PARDER PTCOMA'
+    t[0] =Imprimir(t[3])
+
+def p_instruccion_definicion(t) :
+    'definicion_instr   : NUMERO ID PTCOMA'
+    t[0] =Definicion(t[2])
+
+def p_asignacion_instr(t) :
+    'asignacion_instr   : ID IGUAL expresion_numerica PTCOMA'
+    t[0] =Asignacion(t[1], t[3])
+
+def p_mientras_instr(t) :
+    'mientras_instr     : MIENTRAS PARIZQ expresion_logica PARDER LLAVIZQ instrucciones LLAVDER'
+    t[0] =Mientras(t[3], t[6])
+
+def p_if_instr(t) :
+    'if_instr           : IF PARIZQ expresion_logica PARDER LLAVIZQ instrucciones LLAVDER'
+    t[0] =If(t[3], t[6])
+
+def p_if_else_instr(t) :
+    'if_else_instr      : IF PARIZQ expresion_logica PARDER LLAVIZQ instrucciones LLAVDER ELSE LLAVIZQ instrucciones LLAVDER'
+    t[0] =IfElse(t[3], t[6], t[10])
+
+def p_expresion_binaria(t):
+    '''expresion_numerica : expresion_numerica MAS expresion_numerica
+                        | expresion_numerica MENOS expresion_numerica
+                        | expresion_numerica POR expresion_numerica
+                        | expresion_numerica DIVIDIDO expresion_numerica'''
+    if t[2] == '+'  : t[0] = ExpresionBinaria(t[1], t[3], OPERACION_ARITMETICA.MAS)
+    elif t[2] == '-': t[0] = ExpresionBinaria(t[1], t[3], OPERACION_ARITMETICA.MENOS)
+    elif t[2] == '*': t[0] = ExpresionBinaria(t[1], t[3], OPERACION_ARITMETICA.POR)
+    elif t[2] == '/': t[0] = ExpresionBinaria(t[1], t[3], OPERACION_ARITMETICA.DIVIDIDO)
+
+def p_expresion_unaria(t):
+    'expresion_numerica : MENOS expresion_numerica %prec UMENOS'
+    t[0] = ExpresionNegativo(t[2])
+
+def p_expresion_agrupacion(t):
+    'expresion_numerica : PARIZQ expresion_numerica PARDER'
+    t[0] = t[2]
+
+def p_expresion_number(t):
+    '''expresion_numerica : ENTERO
+                        | DECIMAL'''
+    t[0] = ExpresionNumero(t[1])
+
+def p_expresion_id(t):
+    'expresion_numerica   : ID'
+    t[0] = ExpresionIdentificador(t[1])
+
+def p_expresion_concatenacion(t) :
+    'expresion_cadena     : expresion_cadena CONCAT expresion_cadena'
+    t[0] = ExpresionConcatenar(t[1], t[3])
+
+def p_expresion_cadena(t) :
+    'expresion_cadena     : CADENA'
+    t[0] = ExpresionDobleComilla(t[1])
+
+def p_expresion_cadena_numerico(t) :
+    'expresion_cadena     : expresion_numerica'
+    t[0] = ExpresionCadenaNumerico(t[1])
+
+def p_expresion_logica(t) :
+    '''expresion_logica : expresion_numerica MAYQUE expresion_numerica
+                        | expresion_numerica MENQUE expresion_numerica
+                        | expresion_numerica IGUALQUE expresion_numerica
+                        | expresion_numerica NIGUALQUE expresion_numerica'''
+    if t[2] == '>'    : t[0] = ExpresionLogica(t[1], t[3], OPERACION_LOGICA.MAYOR_QUE)
+    elif t[2] == '<'  : t[0] = ExpresionLogica(t[1], t[3], OPERACION_LOGICA.MENOR_QUE)
+    elif t[2] == '==' : t[0] = ExpresionLogica(t[1], t[3], OPERACION_LOGICA.IGUAL)
+    elif t[2] == '!=' : t[0] = ExpresionLogica(t[1], t[3], OPERACION_LOGICA.DIFERENTE)
+
+def p_error(t):
+    print(t)
+    print("Error sintáctico en '%s'" % t.value)
+
+parser = yacc.yacc()
+
+
+def parse(input) :
+    return parser.parse(input)
+
+# ANALIZADOR SEMANTICO - INSTRUCCIONES
+class Instruccion:
+    '''This is an abstract class'''
+
+class Imprimir(Instruccion) :
+    '''
+        Esta clase representa la instrucción imprimir.
+        La instrucción imprimir únicamente tiene como parámetro una cadena
+    '''
+
+    def __init__(self,  cad) :
+        self.cad = cad
+
+class Mientras(Instruccion) :
+    '''
+        Esta clase representa la instrucción mientras.
+        La instrucción mientras recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera.
+    '''
+
+    def __init__(self, expLogica, instrucciones = []) :
+        self.expLogica = expLogica
+        self.instrucciones = instrucciones
+
+class Definicion(Instruccion) :
+    '''
+        Esta clase representa la instrucción de definición de variables.
+        Recibe como parámetro el nombre del identificador a definir
+    '''
+
+    def __init__(self, id) :
+        self.id = id
+
+class Asignacion(Instruccion) :
+    '''
+        Esta clase representa la instrucción de asignación de variables
+        Recibe como parámetro el identificador a asignar y el valor que será asignado.
+    '''
+
+    def __init__(self, id, expNumerica) :
+        self.id = id
+        self.expNumerica = expNumerica
+
+class If(Instruccion) : 
+    '''
+        Esta clase representa la instrucción if.
+        La instrucción if recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera.
+    '''
+
+    def __init__(self, expLogica, instrucciones = []) :
+        self.expLogica = expLogica
+        self.instrucciones = instrucciones
+
+class IfElse(Instruccion) : 
+    '''
+        Esta clase representa la instrucción if-else.
+        La instrucción if-else recibe como parámetro una expresión lógica y la lista
+        de instrucciones a ejecutar si la expresión lógica es verdadera y otro lista de instrucciones
+        a ejecutar si la expresión lógica es falsa.
+    '''
+
+    def __init__(self, expLogica, instrIfVerdadero = [], instrIfFalso = []) :
+        self.expLogica = expLogica
+        self.instrIfVerdadero = instrIfVerdadero
+        self.instrIfFalso = instrIfFalso
+
+# ANALIZADOR SEMANTICO - EXPRESIONES
+
+class OPERACION_ARITMETICA(Enum) :
+    MAS = 1
+    MENOS = 2
+    POR = 3
+    DIVIDIDO = 4
+
+class OPERACION_LOGICA(Enum) :
+    MAYOR_QUE = 1
+    MENOR_QUE = 2
+    IGUAL = 3
+    DIFERENTE = 4
+
+class ExpresionNumerica:
+    '''
+        Esta clase representa una expresión numérica
+    '''
+
+class ExpresionBinaria(ExpresionNumerica) :
+    '''
+        Esta clase representa la Expresión Aritmética Binaria.
+        Esta clase recibe los operandos y el operador
+    '''
+
+    def __init__(self, exp1, exp2, operador) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.operador = operador
+
+class ExpresionNegativo(ExpresionNumerica) :
+    '''
+        Esta clase representa la Expresión Aritmética Negativa.
+        Esta clase recibe la expresion
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+
+class ExpresionNumero(ExpresionNumerica) :
+    '''
+        Esta clase representa una expresión numérica entera o decimal.
+    '''
+
+    def __init__(self, val = 0) :
+        self.val = val
+
+class ExpresionIdentificador(ExpresionNumerica) :
+    '''
+        Esta clase representa un identificador.
+    '''
+
+    def __init__(self, id = "") :
+        self.id = id
+
+class ExpresionCadena :
+    '''
+        Esta clase representa una Expresión de tipo cadena.
+    '''
+
+class ExpresionConcatenar(ExpresionCadena) :
+    '''
+        Esta clase representa una Expresión de tipo cadena.
+        Recibe como parámetros las 2 expresiones a concatenar
+    '''
+
+    def __init__(self, exp1, exp2) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+
+class ExpresionDobleComilla(ExpresionCadena) :
+    '''
+        Esta clase representa una cadena entre comillas doble.
+        Recibe como parámetro el valor del token procesado por el analizador léxico
+    '''
+
+    def __init__(self, val) :
+        self.val = val
+
+class ExpresionCadenaNumerico(ExpresionCadena) :
+    '''
+        Esta clase representa una expresión numérica tratada como cadena.
+        Recibe como parámetro la expresión numérica
+    '''
+    def __init__(self, exp) :
+        self.exp = exp
+
+class ExpresionLogica() :
+    '''
+        Esta clase representa la expresión lógica.
+        Esta clase recibe los operandos y el operador
+    '''
+
+    def __init__(self, exp1, exp2, operador) :
+        self.exp1 = exp1
+        self.exp2 = exp2
+        self.operador = operador
+
+# EJECUCION PRINCIPAL
+
+def execute_print_statement(instr, ts) :
+    print('> ', resolver_cadena(instr.cad, ts))
+
+def execute_variable_definition(instr, ts) :
+    simbolo = Simbolo(instr.id, TIPO_DATO.NUMERO, 0)      # inicializamos con 0 como valor por defecto
+    ts.agregar(simbolo)
+
+def execute_variable_assignment(instr, ts) :
+    val = resolver_expresion_aritmetica(instr.expNumerica, ts)
+    simbolo = Simbolo(instr.id, TIPO_DATO.NUMERO, val)
+    ts.actualizar(simbolo)
+
+def execute_while_loop(instr, ts) :
+    while resolver_expreision_logica(instr.expLogica, ts) :
+        ts_local = TablaDeSimbolos(ts.simbolos)
+        procesar_instrucciones(instr.instrucciones, ts_local)
+
+def execute_if_statement(instr, ts) :
+    val = resolver_expreision_logica(instr.expLogica, ts)
+    if val :
+        ts_local = TablaDeSimbolos(ts.simbolos)
+        procesar_instrucciones(instr.instrucciones, ts_local)
+
+def execute_if_statement_else(instr, ts) :
+    val = resolver_expreision_logica(instr.expLogica, ts)
+    if val :
+        ts_local = TablaDeSimbolos(ts.simbolos)
+        procesar_instrucciones(instr.instrIfVerdadero, ts_local)
+    else :
+        ts_local = TablaDeSimbolos(ts.simbolos)
+        procesar_instrucciones(instr.instrIfFalso, ts_local)
+
+def resolver_cadena(expCad, ts) :
+    if isinstance(expCad, ExpresionConcatenar) :
+        exp1 = resolver_cadena(expCad.exp1, ts)
+        exp2 = resolver_cadena(expCad.exp2, ts)
+        return exp1 + exp2
+    elif isinstance(expCad, ExpresionDobleComilla) :
+        return expCad.val
+    elif isinstance(expCad, ExpresionCadenaNumerico) :
+        return str(resolver_expresion_aritmetica(expCad.exp, ts))
+    else :
+        print('Error: Expresión cadena no válida')
+
+
+def resolver_expreision_logica(expLog, ts) :
+    exp1 = resolver_expresion_aritmetica(expLog.exp1, ts)
+    exp2 = resolver_expresion_aritmetica(expLog.exp2, ts)
+    if expLog.operador == OPERACION_LOGICA.MAYOR_QUE : return exp1 > exp2
+    if expLog.operador == OPERACION_LOGICA.MENOR_QUE : return exp1 < exp2
+    if expLog.operador == OPERACION_LOGICA.IGUAL : return exp1 == exp2
+    if expLog.operador == OPERACION_LOGICA.DIFERENTE : return exp1 != exp2
+
+def resolver_expresion_aritmetica(expNum, ts) :
+    if isinstance(expNum, ExpresionBinaria) :
+        exp1 = resolver_expresion_aritmetica(expNum.exp1, ts)
+        exp2 = resolver_expresion_aritmetica(expNum.exp2, ts)
+        if expNum.operador == OPERACION_ARITMETICA.MAS : return exp1 + exp2
+        if expNum.operador == OPERACION_ARITMETICA.MENOS : return exp1 - exp2
+        if expNum.operador == OPERACION_ARITMETICA.POR : return exp1 * exp2
+        if expNum.operador == OPERACION_ARITMETICA.DIVIDIDO : return exp1 / exp2
+    elif isinstance(expNum, ExpresionNegativo) :
+        exp = resolver_expresion_aritmetica(expNum.exp, ts)
+        return exp * -1
+    elif isinstance(expNum, ExpresionNumero) :
+        return expNum.val
+    elif isinstance(expNum, ExpresionIdentificador) :
+        return ts.obtener(expNum.id).valor
+
+
+def procesar_instrucciones(instrucciones, ts) :
+    ## lista de instrucciones recolectadas
+    for instr in instrucciones :
+        if isinstance(instr, Imprimir) : execute_print_statement(instr, ts)
+        elif isinstance(instr, Definicion) : execute_variable_definition(instr, ts)
+        elif isinstance(instr, Asignacion) : execute_variable_assignment(instr, ts)
+        elif isinstance(instr, Mientras) : execute_while_loop(instr, ts)
+        elif isinstance(instr, If) : execute_if_statement(instr, ts)
+        elif isinstance(instr, IfElse) : execute_if_statement_else(instr, ts)
+        else : print('Error: instrucción no válida')
+
+f = open("./entrada.txt", "r")
+input = f.read()
+
+instrucciones = parse(input)
+ts_global = TablaDeSimbolos()
+
+procesar_instrucciones(instrucciones, ts_global)
+

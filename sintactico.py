b995a065e63aca215a83e5f42dcf21ebb63ef9d5
CloudySpace <yannick.becker25@gmail.com>
2023-08-03 18:32:49 -0600

    Added some elifs

--- sintactico.py
+++ sintactico.py
@@ -42,21 +42,36 @@ def parse_long_declaration(input_string):
     tokens = re.findall(r'\w+|\S', input_string)
     if tokens[0] != "long":
         return None
-    variable_name = tokens[1]
-    initial_value = None
-    if len(tokens) > 3 and tokens[2] == "=":
-        initial_value = tokens[3]
-    return ("long", variable_name, initial_value)
+
+    # Check if the second token is a valid identifier (variable name) or constant
+    if not re.match(r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', tokens[1]) and not is_valid_constant(tokens[1]):
+        return None
+
+    # Check if there is an assignment operator and a value after the identifier
+    if len(tokens) > 2 and tokens[2] == '=':
+        # Join the tokens after the assignment operator to get the initialization value
+        initial_value = ' '.join(tokens[3:])
+    else:
+        initial_value = None
+
 
 def parse_short_declaration(input_string):
     tokens = re.findall(r'\w+|\S', input_string)
     if tokens[0] != "short":
         return None
-    variable_name = tokens[1]
-    initial_value = None
-    if len(tokens) > 3 and tokens[2] == "=":
-        initial_value = tokens[3]
-    return ("short", variable_name, initial_value)
+
+    # Check if the second token is a valid identifier (variable name) or constant
+    if not re.match(r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', tokens[1]) and not is_valid_constant(tokens[1]):
+        return None
+
+    # Check if there is an assignment operator and a value after the identifier
+    if len(tokens) > 2 and tokens[2] == '=':
+        # Join the tokens after the assignment operator to get the initialization value
+        initial_value = ' '.join(tokens[3:])
+    else:
+        initial_value = None
+
+    return ("short", tokens[1], initial_value)
 
 def parse_declaration(tokens):
     variable_type = tokens[0]
@@ -160,7 +175,7 @@ def extract_for_parts(tokens):
 
     return initialization_tokens, condition_tokens, update_tokens, index
 
-ddef parse_const_declaration(tokens):
+def parse_const_declaration(tokens):
     if tokens[0] != 'const':
         raise SyntaxError("Expected 'const'")
     
@@ -213,6 +228,66 @@ def parse_body(tokens):
     # Retornar la lista de nodos que representan el cuerpo de código
     return body_nodes
 
+def is_declaration(tokens):
+    # List of valid data types
+    valid_types = ['int', 'float', 'char']
+    # List of valid modifiers
+    valid_modifiers = ['short', 'long']
+
+    if len(tokens) < 2:
+        return False
+
+    # Check if the first token is a valid modifier (optional)
+    if tokens[0] in valid_modifiers:
+        tokens = tokens[1:]
+
+    # Check if the first token is a valid data type
+    if tokens[0] not in valid_types:
+        return False
+
+    # Check if the second token is a valid identifier (variable name) or constant
+    if not re.match(r'\b[a-zA-Z_][a-zA-Z0-9_]*\b', tokens[1]) and not is_valid_constant(tokens[1]):
+        return False
+
+    # Check if there is an assignment operator and a value after the identifier
+    if len(tokens) > 2 and tokens[2] == '=' and len(tokens) < 4:
+        return False
+
+    return True
+
+def is_valid_constant(token):
+    # Regular expression pattern for numeric constants
+    pattern_numeric = r'\b\d+(\.\d*)?\b'
+    # Regular expression pattern for character constants
+    pattern_character = r'\'[^\\\']*\''
+
+    # Check if the token matches any of the constant patterns
+    return re.match(pattern_numeric, token) or re.match(pattern_character, token)
+
+def is_control_structure(tokens):
+    # List of valid control structure keywords
+    valid_keywords = ['for', 'while', 'if']
+
+    if len(tokens) < 2:
+        return False
+
+    # Check if the first token is a valid control structure keyword
+    if tokens[0] not in valid_keywords:
+        return False
+
+    # Check if there is a valid condition inside parentheses for 'if' and 'while'
+    if tokens[0] in ['if', 'while']:
+        if len(tokens) < 3 or tokens[1] != '(' or tokens[-1] != ')':
+            return False
+
+    # Check if there is a valid initialization, condition, and update for 'for'
+    if tokens[0] == 'for':
+        if len(tokens) < 6 or tokens[1] != '(' or tokens[3] != ';' or tokens[-2] != ')':
+            return False
+
+    return True
+
+
 def parse_case_or_default(tokens):
     if tokens[0] == 'case':
         case_expression = parse_expression(tokens[1:]) # Analiza la expresión del caso
@@ -263,7 +338,7 @@ def parse_assignment(tokens):
     value_tokens = tokens[2:]
 
     # Verificamos el operador de asignación
-    if tokens[1] != '=':
+    if tokens[2] != '=':
         raise SyntaxError(f"Expected '=', got '{tokens[1]}'")
 
     # Analizamos la variable y el valor (puede ser una expresión)
@@ -289,6 +364,86 @@ def parse_if_statement(tokens):
     if else_tokens and else_tokens[0] == 'else':
         parse_else_statement(else_tokens[1:])
 
+def extract_parentheses_content(input_string):
+    # Initializing variables
+    stack = []
+    result = []
+
+    # Iterating over the characters in the input string
+    for char in input_string:
+        if char == '(':
+            # If an opening parenthesis is encountered, push it onto the stack
+            stack.append(char)
+        elif char == ')':
+            # If a closing parenthesis is encountered, pop characters from the stack
+            # until an opening parenthesis is found
+            if stack:
+                stack.pop()
+                if not stack:
+                    # If the stack is empty, it means we have found the closing parenthesis
+                    # for the content we want to extract. So, we add the characters to the result.
+                    result.append(char)
+            else:
+                # If there's no matching opening parenthesis in the stack, raise an error
+                raise SyntaxError("Mismatched parentheses in the input string")
+        else:
+            # If the character is not a parenthesis, add it to the result
+            if stack:
+                # If we are inside the parentheses, add the character to the result
+                result.append(char)
+
+    # After processing all characters, join the result list to get the content inside the parentheses as a string
+    return ''.join(result)
+
+def parse_condition(tokens):
+    # Extracting the content inside the parentheses
+    condition_content = extract_parentheses_content(tokens)
+    
+    # Parsing the expression inside the parentheses
+    parse_expression(condition_content)
+
+def extract_braces_content(input_string):
+    # Initializing variables
+    stack = []
+    result = []
+
+    # Iterating over the characters in the input string
+    for char in input_string:
+        if char == '{':
+            # If an opening brace is encountered, push it onto the stack
+            stack.append(char)
+            if len(stack) > 1:
+                # If this is not the first opening brace, add it to the result
+                result.append(char)
+        elif char == '}':
+            # If a closing brace is encountered, pop characters from the stack
+            # until an opening brace is found
+            if stack:
+                stack.pop()
+                if not stack:
+                    # If the stack is empty, it means we have found the closing brace
+                    # for the content we want to extract. So, we stop iterating.
+                    break
+            else:
+                # If there's no matching opening brace in the stack, raise an error
+                raise SyntaxError("Mismatched braces in the input string")
+        else:
+            # If the character is not a brace, add it to the result
+            if len(stack) > 1:
+                # If we are inside the outermost braces, add the character to the result
+                result.append(char)
+
+    # After processing all characters, join the result list to get the content inside the braces as a string
+    return ''.join(result)
+
+def parse_else_statement(tokens):
+    # Extracting the content inside the curly braces
+    else_content = extract_braces_content(tokens)
+    
+    # Parsing the body of the "else" statement
+    parse_body(re.findall(r'\w+|\S', else_content))
+
+
 def parse_logical_expression(tokens):
     # La expresión puede incluir operadores lógicos
     index = 0
@@ -310,6 +465,14 @@ def parse_sizeof(tokens):
     # Verificamos que el argumento sea una variable o constante válida
     if not (is_valid_variable(argument_token) or is_valid_constant(argument_token)):
         raise SyntaxError(f"Invalid argument for sizeof: '{argument_token}'")
+
+def is_valid_variable(variable_name):
+    # Regular expression pattern for valid variable names
+    pattern_variable = r'\b[a-zA-Z_][a-zA-Z0-9_]*\b'
+
+    # Check if the variable name matches the valid variable pattern
+    return re.match(pattern_variable, variable_name) is not None
+
 
 def parse_cases(tokens):
     index = 0
@@ -343,6 +506,41 @@ def parse_switch_statement(tokens):
     # Extraemos y analizamos los casos
     case_tokens = extract_body_until_closing_brace(tokens[index + 1:])
     parse_cases(case_tokens)
+
+def extract_body_until_closing_brace(tokens):
+    # Initializing variables
+    stack = []
+    result = []
+
+    # Iterating over the characters in the input tokens
+    for token in tokens:
+        if token == '{':
+            # If an opening brace is encountered, push it onto the stack
+            stack.append(token)
+            if len(stack) > 1:
+                # If this is not the first opening brace, add it to the result
+                result.append(token)
+        elif token == '}':
+            # If a closing brace is encountered, pop characters from the stack
+            # until an opening brace is found
+            if stack:
+                stack.pop()
+                if not stack:
+                    # If the stack is empty, it means we have found the closing brace
+                    # for the content we want to extract. So, we stop iterating.
+                    break
+            else:
+                # If there's no matching opening brace in the stack, raise an error
+                raise SyntaxError("Mismatched braces in the input tokens")
+        else:
+            # If the token is not a brace, add it to the result
+            if len(stack) > 1:
+                # If we are inside the outermost braces, add the token to the result
+                result.append(token)
+
+    # After processing all tokens, return the result list containing the content inside the braces
+    return result
+
 
 def parse_constant(token):
     # Expresión regular para constantes numéricas enteras y flotantes
@@ -429,3 +627,47 @@ def parse_return_statement(tokens):
     # Extraemos y analizamos la expresión a devolver
     expression_tokens = tokens[1:]
     parse_expression(expression_tokens)
+
+
+def main():
+    expressions = [
+        "int opcion;",
+        "float opcion2 = 0.0;",
+        "char var = 'W';",
+        "const char var = 'W';",
+        "const char var = '\n';",
+        "long int opcion3;",
+        "short int opcion4 = 1;",
+        "while(i > 3){\n\tcout << i << endl;\n\ti++;\n}",
+        "if(opcion >= opcion2);\nif(opcion >= opcion2);\nelse\n\treturn;",
+        "int var = sizeof(var / const);",
+        "for (i = 1; i <= 10; ++i) {\n\tif (i == 3)\n\t\tcontinue;\n\tif (i == 7)\n\t\tbreak;\n}",
+        "switch(expression) {\n\tdefault:\n\t\t// some statements to execute when default;\n\tcase '0':\n\t\t// some statements to execute when 1\n\t\tbreak;\n\tcase '1':\n\t\t// some statements to execute when 5\n\t\tbreak;\n}",
+        "int i;\ndo {\n\tcout << i;\n\ti++;\n} while (i < 10);"
+    ]
+
+    for expression in expressions:
+        tokens = re.findall(r'\w+|\S', expression)
+        if tokens[0] == 'int':
+            result = parse_int_declaration(expression)
+        elif tokens[0] == 'float':
+            result = parse_float_declaration(expression)
+        elif tokens[0] == 'char':
+            result = parse_char_declaration(expression)
+        elif tokens[0] == 'long':
+            result = parse_long_declaration(expression)
+        elif tokens[0] == 'short':
+            result = parse_short_declaration(expression)
+        elif tokens[0] == 'while':
+            result = is_control_structure(expression)
+        elif tokens[0] == 'for':
+            result = is_control_structure
+        
+        else:
+            result = None
+
+        print(f"Expression: {expression}\nAST: {result}\n")
+
+
+if __name__ == "__main__":
+    main()
